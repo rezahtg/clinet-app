6b449869436004d418cd47ca232de7acd2d5948f
rezahtg <reza.hutagaol@students.amikom.ac.id>
2021-08-03 13:05:52 +0700

    fix create employee

--- src/main/java/com/mii/clinetapp/models/Employee.java
+++ src/main/java/com/mii/clinetapp/models/Employee.java
@@ -18,15 +18,5 @@ public class Employee implements Serializable {
     private String lastName;
     private String email;
     private String address;
-    private Long department;
-    private User user;
-    public List<Project> projects;
-
-    public Employee(String firstName, String lastName, String email, String address, Long department) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.address = address;
-        this.department = department;
-    }
+    private Department department;
 }
